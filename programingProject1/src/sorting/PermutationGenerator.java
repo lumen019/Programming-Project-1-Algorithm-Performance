f2b2e12961ac50a8ba35d3dfba1566a25421ca12
Lumen019 <237933659+lumen019@users.noreply.github.com>
2026-09-17 01:15:38 -0600

    modified perm generator

--- programingProject1/src/sorting/PermutationGenerator.java
+++ programingProject1/src/sorting/PermutationGenerator.java
@@ -1,12 +1,22 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class PermutationGenerator {
 
-	public static ArrayList<>
+	public static void generatePerms(int n) {
+		int[] numbers = new int [n];
+		
+		for(int i = 0; i < n; i++) {
+			numbers[i] =i;
+		}
+		
+		System.out.println(Arrays.toString(numbers));
+	}
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		generatePerms(3);
 
 	}
 
