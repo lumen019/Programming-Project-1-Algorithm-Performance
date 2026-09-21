d24caf3c53bda25cb457ffba5e69a6080b87245e
Lumen019 <237933659+lumen019@users.noreply.github.com>
2026-09-20 21:11:18 -0600

    updated shakersort

--- programingProject1/src/sorting/ShakerSort.java
+++ programingProject1/src/sorting/ShakerSort.java
@@ -1,5 +1,21 @@
 package sorting;
 
 public class ShakerSort {
-
+	
+	public static void shakerSort (int[] numbers) {
+		int start = 0;
+		int end = numbers.length -1;
+		boolean swapped = true;
+		
+		while (swapped) {
+			swapped = false;
+			
+			for(int i = start; i< end; i++) {
+				if(numbers[i] > numbers[i+1]) {
+					
+				}
+			}
+		}	
+	}
+		
 }
