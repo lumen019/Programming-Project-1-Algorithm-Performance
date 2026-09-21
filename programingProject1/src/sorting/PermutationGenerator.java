b69feba75562ea2356b8a7e32c829b2fc59bef25
Lumen019 <237933659+lumen019@users.noreply.github.com>
2026-09-20 20:44:37 -0600

    update perms gen

--- programingProject1/src/sorting/PermutationGenerator.java
+++ programingProject1/src/sorting/PermutationGenerator.java
@@ -13,14 +13,13 @@ public class PermutationGenerator {
 		
 		System.out.println(Arrays.toString(numbers));
 		
-		PermutationGenerator generate = new PermutationGenerator();
 		
 		int count = 0;
 		
 		//only works by manually changing count for now for different n's
 		//TODO
 		while (count < 23) {
-			generate.generatePerms(numbers);
+			generatePerms(numbers);
 			System.out.println(Arrays.toString(numbers));
 			count++;
 		}
@@ -47,7 +46,7 @@ public class PermutationGenerator {
 	 * 
 	 * @param numbers
 	 */
-	public  void generatePerms(int[] numbers) {
+	public static void generatePerms(int[] numbers) {
 		int i = numbers.length - 2;  //2nd to last element of array
 		
 		//finds pivot i 
@@ -73,13 +72,13 @@ public class PermutationGenerator {
 
 	}
 	
-	public void swap (int[] numbers, int i, int j) {
+	public static void swap (int[] numbers, int i, int j) {
 		int temp = numbers[i];
 		numbers[i] = numbers[j];
 		numbers[j] = temp;
 	}
 	
-	public void reverse (int[] numbers, int start) {
+	public static void reverse (int[] numbers, int start) {
 		int end = numbers.length - 1;
 		while (start < end) {
 			swap(numbers, start, end);
